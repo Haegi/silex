fef31eb29d5dd8e3cde0b672eaa00b3239bee552
D070374 <benjamin.haegenlaeuer@sap.com>
2019-03-21 22:10:27 +0100

    try to fix mongodb url error

--- src/dbcontroller.spec.ts
+++ src/dbcontroller.spec.ts
@@ -8,7 +8,7 @@ import DBWrapper from "./dbwrapper";
   private classUnderTest: DatabaseController;
 
   private async before(): Promise<void> {
-    this.classUnderTest = await DBWrapper.getInstance("127.0.0.1:43448", "test");
+    this.classUnderTest = await DBWrapper.getInstance("127.0.0.1:27017", "test");
   }
   private afterAll(): void {
     this.classUnderTest.close();
@@ -17,7 +17,7 @@ import DBWrapper from "./dbwrapper";
   @test private async testDBconnection(): Promise<void> {
     await this.classUnderTest.connect();
     const connection: string = await this.classUnderTest.mongodburl;
-    assert.equal(connection, "mongodb://127.0.0.1:43448/test");
+    assert.equal(connection, "mongodb://127.0.0.1:27017/test");
   }
 
   @test private async testChangeCollection(): Promise<void> {
