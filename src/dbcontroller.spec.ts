7a92d82f8cbba307763e58f149cb72e53833bc0a
D070374 <benjamin.haegenlaeuer@sap.com>
2019-04-22 12:39:33 +0200

    changed to static

--- src/dbcontroller.spec.ts
+++ src/dbcontroller.spec.ts
@@ -17,14 +17,14 @@ import { DatabaseController, IMessage } from "./dbcontroller";
   }*/
 
   @test private async testDBconnection(): Promise<void> {
-    await this.classUnderTest.connect();
-    const connection: string = await this.classUnderTest.mongodburl;
+    await DBControllerTests.classUnderTest.connect();
+    const connection: string = await DBControllerTests.classUnderTest.mongodburl;
     assert.equal(connection, "mongodb://127.0.0.1:27017");
   }
 
   @test private async testChangeCollection(): Promise<void> {
-    await this.classUnderTest.changeColl("ml");
-    const collName: string =  this.classUnderTest.getCollectionName();
+    await DBControllerTests.classUnderTest.changeColl("ml");
+    const collName: string =  DBControllerTests.classUnderTest.getCollectionName();
     assert.equal(collName, "ml");
 
   }
@@ -44,8 +44,8 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                               content: {
                                 value: "This is the first message",
                               } };
-    await this.classUnderTest.insert(value);
-    const values: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.insert(value);
+    const values: JSON = await DBControllerTests.classUnderTest.findAll();
     const actual: string = values[0].topic;
     assert.equal(actual, "Unittest 1");
   }
@@ -59,30 +59,30 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                               content: {
                                 value: "This is the second message",
                               } };
-    await this.classUnderTest.insert(value);
-    const values: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.insert(value);
+    const values: JSON = await DBControllerTests.classUnderTest.findAll();
     const actual: string = values[1].topic;
     assert.equal(actual, "Unittest 2");
   }
 
   @test private async testFindOne(): Promise<void> {
     const searchSchema: {} = {deviceID: "2"};
-    const value: JSON = await this.classUnderTest.find(searchSchema);
+    const value: JSON = await DBControllerTests.classUnderTest.find(searchSchema);
     const actual: number = Object.keys(value).length;
     assert.equal(actual, 1);
   }
 
   @test private async testSort(): Promise<void> {
     const sortSchema: {} = {deviceID: -1};
-    const values: JSON = await this.classUnderTest.sort(sortSchema);
+    const values: JSON = await DBControllerTests.classUnderTest.sort(sortSchema);
     const actual: string = values[0].deviceID;
     assert.equal(actual, "2");
   }
 
   @test private async testDeleteOne(): Promise<void> {
     const query: {} = {topic: "Unittest 2"};
-    await this.classUnderTest.deleteOne(query);
-    const value: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.deleteOne(query);
+    const value: JSON = await DBControllerTests.classUnderTest.findAll();
     const actual: number = Object.keys(value).length;
     assert.equal(actual, 1);
   }
@@ -97,7 +97,7 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                               content: {
                                 value: "20",
                               } };
-    await this.classUnderTest.insert(value1);
+    await DBControllerTests.classUnderTest.insert(value1);
 
     const value2: IMessage = { topic: "Temperature",
                               // tslint:disable-next-line:object-literal-sort-keys
@@ -107,15 +107,15 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                               content: {
                                 value: "200",
                               } };
-    await this.classUnderTest.insert(value2);
-    await this.classUnderTest.deleteMany(query);
-    const message: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.insert(value2);
+    await DBControllerTests.classUnderTest.deleteMany(query);
+    const message: JSON = await DBControllerTests.classUnderTest.findAll();
     const actual: number = Object.keys(message).length;
     assert.equal(actual, 1);
   }
 
   @test private async test2Collections() {
-    await this.classUnderTest.changeColl("IoT");
+    await DBControllerTests.classUnderTest.changeColl("IoT");
     const insertValue: IMessage = { topic: "Test",
                               // tslint:disable-next-line:object-literal-sort-keys
                               deviceID: "1",
@@ -124,12 +124,12 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                               content: {
                                 value: "1",
                               } };
-    await this.classUnderTest.insert(insertValue);
-    await this.classUnderTest.changeColl("ml");
-    const newCollValue: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.insert(insertValue);
+    await DBControllerTests.classUnderTest.changeColl("ml");
+    const newCollValue: JSON = await DBControllerTests.classUnderTest.findAll();
     const newCollLength: number = Object.keys(newCollValue).length;
-    await this.classUnderTest.changeColl("IoT");
-    const IoTValue: JSON = await this.classUnderTest.findAll();
+    await DBControllerTests.classUnderTest.changeColl("IoT");
+    const IoTValue: JSON = await DBControllerTests.classUnderTest.findAll();
     const IoTLength: number = Object.keys(IoTValue).length;
     const actual: number = newCollLength + IoTLength;
     assert.equal(actual, 2);
@@ -152,10 +152,10 @@ import { DatabaseController, IMessage } from "./dbcontroller";
                                     content: {
                                       value: "2",
                                     } };
-    await this.classUnderTest.insert(insertValue1);
-    await this.classUnderTest.insert(insertValue2);
+    await DBControllerTests.classUnderTest.insert(insertValue1);
+    await DBControllerTests.classUnderTest.insert(insertValue2);
     const sortSchema: {} = {deviceID: -1};
-    const values: JSON = await this.classUnderTest.sort(sortSchema, undefined, 2);
+    const values: JSON = await DBControllerTests.classUnderTest.sort(sortSchema, undefined, 2);
     const actual: number = Object.keys(values).length;
     assert.equal(actual, 2);
   }
